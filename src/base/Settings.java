45dc5286120484d872e2acfd9d62a69e956d9ecb
Gabriel Cespedes <gcespedes@overactiveinc.com>
2019-06-04 18:06:25 -0300

    First stating commit.

---
+++ src/base/Settings.java
@@ -0,0 +1,11 @@
+package base;
+
+public class Settings {
+
+    public static final String BASE_URL = "https://www.facebook.com/";
+
+    public static final String RESOURCES = "src/resources/";
+    public static final String CHROMEDRIVER =  "chromedriver.exe";
+
+
+}
--- src/tests/FirstTests.java
+++ src/tests/FirstTests.java
@@ -5,9 +5,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import org.openqa.selenium.support.FindBy;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static base.Settings.*;
 
 public class FirstTests {
 
@@ -24,22 +26,42 @@ public class FirstTests {
     @FindBy(xpath = "//input[@value='Log In']")
     public WebElement buttonLogIn;
 
-    @Test
-        public void firstExampleTest() throws InterruptedException {
-            System.setProperty("webdriver.chrome.driver", "C:\\Users\\gcespedes\\xpoprojects\\exampleone\\src\\resources\\chromedriver.exe");
-
-            chromeOptions = new ChromeOptions();
-            chromeOptions.addArguments("start-maximized");
-            driver = new ChromeDriver(chromeOptions);
-
-            driver.navigate().to("https://www.facebook.com/");
-
-            driver.findElement(By.id("email")).sendKeys("someemail@address.com");
-            driver.findElement(By.id("pass")).sendKeys("pass");
-            driver.findElement(By.xpath("//input[@value='Log In']")).click();
-
-            Thread.sleep(3000);
-
-            driver.quit();
+    @BeforeSuite
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", RESOURCES + CHROMEDRIVER);
+        chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("start-maximized");
+        driver = new ChromeDriver(chromeOptions);
     }
+
+    @AfterSuite
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @BeforeMethod
+    public void navigateToBaseUrl(){
+        driver.navigate().to(BASE_URL);
+    }
+
+    @Test
+    public void firstTest() throws InterruptedException {
+
+        driver.findElement(By.id("email")).sendKeys("someemail@address.com");
+        driver.findElement(By.id("pass")).sendKeys("pass");
+        driver.findElement(By.xpath("//input[@value='Log In']")).click();
+        Thread.sleep(3000);
+
+    }
+
+    @Test
+    public void secondTest() throws InterruptedException {
+
+        driver.findElement(By.id("email")).sendKeys("someemail@address.com");
+        driver.findElement(By.id("pass")).sendKeys("pass");
+        driver.findElement(By.xpath("//input[@value='Log In']")).click();
+        Thread.sleep(3000);
+
+    }
+
 }
